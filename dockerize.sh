2b958a290bf72a44707ab2b9ecdf08e5f53b62b3
Kyle Hopfer <Kyle.Hopfer@gmail.com>
2017-10-01 23:03:25 -0500

    Deployment changes

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn clean package
 docker build -t gpls-api:v0.1.0 .
-docker run --restart=always --name gpls-api -p 8080:8080 -d gpls-api:v0.1.0
+#docker run --restart=always --name gpls-api -p 8080:8080 -d gpls-api:v0.1.0
