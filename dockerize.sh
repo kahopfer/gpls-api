2b8c5380d9752ccdcfec57a91d33c3352d7a96e9
Kyle Hopfer <Kyle.Hopfer@gmail.com>
2017-10-28 20:57:06 -0500

    Added docker save to dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mvn clean package
 docker build -t gpls-api:v0.1.0 .
+docker save -o /Users/kylehopfer/Desktop/gpls-api.tar.gz gpls-api
 #docker run --restart=always --name gpls-api -p 8080:8080 -d gpls-api:v0.1.0
